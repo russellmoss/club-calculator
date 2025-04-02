45123f590d4d063247f966dd5bf94d9c7ba37f06
Russsell <russell@mileaestatevineyard.com>
2025-04-02 00:25:29 -0400

    fix: restore calculator flow in routing

--- src/App.jsx
+++ src/App.jsx
@@ -1,33 +1,33 @@
 import React from 'react';
 import { BrowserRouter as Router, Routes, Route, Navigate } from 'react-router-dom';
-import { CalculatorProvider } from './contexts/CalculatorContext';
 import Header from './components/common/Header';
 import WelcomePage from './components/calculator/WelcomePage';
-import ResultsPage from './components/calculator/ResultsPage';
+import CalculatorFlow from './components/calculator/CalculatorFlow';
 import ClubSelectionPage from './components/clubSignup/ClubSelectionPage';
 import Commerce7ApiTest from './components/clubSignup/Commerce7ApiTest';
 import { ToastProvider } from './contexts/ToastContext';
+import { CalculatorProvider } from './contexts/CalculatorContext';
 
 function App() {
   return (
-    <ToastProvider>
+    <Router>
       <CalculatorProvider>
-        <Router>
-          <div className="min-h-screen bg-gray-50">
+        <ToastProvider>
+          <div className="min-h-screen bg-background">
             <Header />
-            <main className="container mx-auto py-8 px-4">
+            <main className="container mx-auto px-4 py-8">
               <Routes>
                 <Route path="/" element={<WelcomePage />} />
-                <Route path="/calculator" element={<ResultsPage />} />
+                <Route path="/calculator" element={<CalculatorFlow />} />
                 <Route path="/signup" element={<ClubSelectionPage />} />
                 <Route path="/signup/form" element={<Navigate to="/signup" replace />} />
-                <Route path="/api-test" element={<Commerce7ApiTest />} />
+                <Route path="/test" element={<Commerce7ApiTest />} />
               </Routes>
             </main>
           </div>
-        </Router>
+        </ToastProvider>
       </CalculatorProvider>
-    </ToastProvider>
+    </Router>
   );
 }
 
