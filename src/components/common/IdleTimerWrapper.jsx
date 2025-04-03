a6571aef97b96f9aeebe4f6c41fd04d62660a4f1
Russsell <russell@mileaestatevineyard.com>
2025-04-03 12:12:55 -0400

    Fix React Hooks rule violation in IdleTimerWrapper

--- src/components/common/IdleTimerWrapper.jsx
+++ src/components/common/IdleTimerWrapper.jsx
@@ -7,10 +7,11 @@ const IdleTimerWrapper = ({ children, onIdle }) => {
   const location = useLocation();
   const isStartPage = location.pathname === '/';
   
-  // Only use the idle timer if we're not on the start page
+  // Always call the hook, but only use its values if not on start page
+  const idleTimer = useIdleTimer(onIdle, 180000);
   const { timeLeft, reset, isIdle } = isStartPage 
     ? { timeLeft: 0, reset: () => {}, isIdle: false }
-    : useIdleTimer(onIdle, 180000);
+    : idleTimer;
 
   return (
     <>
